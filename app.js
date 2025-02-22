1fb927b8c8eb0b7f0ebe7dcd87276008d663dc04
Archana Sankar <your-email@example.com>
2025-02-22 09:52:52 +0530

    project initialize

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ const app=express();
 const env=require("dotenv").config();
 const db=require('./config/db');
 db()
+const userRouter=require("./routes/userRouter")
+
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.set("view engine","ejs");
+app.set("views",[this.path.join(__dirname,'views/user')])
+app.use(express.static(__dirname,"public"));
+
+
+
+app.use("/",userRouter)
+
+
+
 
 
 app.listen(process.env.PORT,()=>{
