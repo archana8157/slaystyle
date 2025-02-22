09dad8db46f1610164215ccee959fb2169d6f61d
Archana Sankar <your-email@example.com>
2025-02-22 12:11:27 +0530

    view engine set up

---
+++ controllers/user/userController.js
@@ -0,0 +1,34 @@
+
+const pageNotFound=async (req,res)=>{
+    try{
+         res.render("page-404")
+    }catch(error){
+      res.redirect("/pageNotFound")
+    }
+}
+
+
+
+
+
+
+const loadHomepage=async (req,res)=>{
+    try{
+        return res.render("home");
+    }catch(error){
+        console.log("homepage not found")
+        res.send(500).send("server error")
+    }
+}
+
+
+
+
+
+
+
+
+module.exports={
+    loadHomepage,
+    pageNotFound,
+}
